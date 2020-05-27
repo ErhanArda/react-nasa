c06f19afbb5d8ec34d3d103213f256001a33f175
Erhan Arda <erhanarda7@gmail.com>
2020-05-28 01:20:07 +0300

    Updates

---
+++ precache-manifest.a616533b44e5f4ca3e8f6cc90c1b7c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e76c0611ea303a3734fbc4a4508603",
+    "url": "/react-nasa/index.html"
+  },
+  {
+    "revision": "8695d155016ff1c67ace",
+    "url": "/react-nasa/static/css/main.cbdff6c0.chunk.css"
+  },
+  {
+    "revision": "40d4a8cc5bf121ab043d",
+    "url": "/react-nasa/static/js/2.b868f704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-nasa/static/js/2.b868f704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8695d155016ff1c67ace",
+    "url": "/react-nasa/static/js/main.094e65f5.chunk.js"
+  },
+  {
+    "revision": "764694251ab5fafb6191",
+    "url": "/react-nasa/static/js/runtime-main.63a6c6f9.js"
+  },
+  {
+    "revision": "8a423626190b96d351d82507ee533184",
+    "url": "/react-nasa/static/media/S2-3840x2160.8a423626.jpg"
+  }
+]);
